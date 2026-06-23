1d68bedcc962879d11d9af92a5e87a16c8b24595
José Pablicio Sousa Viana <pabliciosousa49@gmail.com>
2026-06-23 19:03:28 -0300

    criei o endpoint /quiz do tipo POST

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
-import { analyzeCode } from '../controllers/aiController.js'
+import { analyzeCode, generateQuiz } from '../controllers/aiController.js'
 
 const router = Router()
 
 // Rota POST que o frontend vai acessar para enviar o código
 router.post('/analyze', analyzeCode)
+
+// Rota POST que o frontend vai acessar para gerar o quiz de 20 questões
+router.post('/quiz', generateQuiz)
 
 export default router
